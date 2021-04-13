3486ed3147f8f9e07d1ddd197fddb3ae80ddbb22
Ruris <ruris@qq.com>
2021-04-13 11:14:33 +0800

    添加 math 模块

---
+++ LLGeneral/Classes/Math/Clampable.swift
@@ -0,0 +1,100 @@
+//
+//  Clampable.swift
+//  Pods-SwiftMath_Example
+//
+//  Created by ZHK on 2020/9/2.
+//  
+//
+
+import Foundation
+
+public protocol Clampable: Comparable {
+    
+    /// 把对象限制在给定区间内
+    /// - Parameter range: 指定的限制区间
+    func clamp(_ range: ClosedRange<Self>) -> Self
+    
+    /// 把对象限制在指定的最小最大值范围内
+    /// - Parameters:
+    ///   - min: 最小值
+    ///   - max: 最大值
+    func clamp(min: Self, max: Self) -> Self
+    
+    /// 把 value 限定在给定区间内
+    /// - Parameters:
+    ///   - value: 指定值
+    ///   - range: 范围区间
+    static func clamp(value: Self, range: ClosedRange<Self>) -> Self
+    
+    /// 把 value 限定在指定的最小最大值范围内
+    /// - Parameters:
+    ///   - value: 给定的值
+    ///   - min:   最小值
+    ///   - max:   最大值
+    static func clamp(value: Self, min: Self, max: Self) -> Self
+}
+
+public extension Clampable {
+    
+    func clamp(_ range: ClosedRange<Self>) -> Self {
+        Self.clamp(value: self, min: range.lowerBound, max: range.upperBound)
+    }
+    
+    func clamp(min: Self, max: Self) -> Self {
+        Self.clamp(value: self, min: min, max: max)
+    }
+    
+    static func clamp(value: Self, range: ClosedRange<Self>) -> Self {
+        clamp(value: value, min: range.lowerBound, max: range.upperBound)
+    }
+    
+    static func clamp(value: Self, min: Self, max: Self) -> Self {
+        if value < min { return min }
+        if value > max { return max }
+        return value
+    }
+}
+
+extension Int: Clampable {}
+
+extension Int8: Clampable {}
+
+extension Int16: Clampable {}
+
+extension Int32: Clampable {}
+
+extension Int64: Clampable {}
+
+extension UInt: Clampable {}
+
+extension UInt8: Clampable {}
+
+extension UInt16: Clampable {}
+
+extension UInt32: Clampable {}
+
+extension UInt64: Clampable {}
+
+// Float = Float32
+extension Float: Clampable {}
+
+// Double = Float64
+extension Double: Clampable {}
+
+extension Decimal: Clampable {}
+
+extension Character: Clampable {}
+
+#if os(iOS)
+
+extension CGFloat: Clampable {}
+
+#endif
+
+#if os(macOS)
+
+extension CGFloat: Clampable {}
+
+extension Float80: Clampable {}
+    
+#endif
---
+++ README.md
@@ -0,0 +1,29 @@
+# LLGeneral
+
+[![CI Status](https://img.shields.io/travis/ruris/LLGeneral.svg?style=flat)](https://travis-ci.org/ruris/LLGeneral)
+[![Version](https://img.shields.io/cocoapods/v/LLGeneral.svg?style=flat)](https://cocoapods.org/pods/LLGeneral)
+[![License](https://img.shields.io/cocoapods/l/LLGeneral.svg?style=flat)](https://cocoapods.org/pods/LLGeneral)
+[![Platform](https://img.shields.io/cocoapods/p/LLGeneral.svg?style=flat)](https://cocoapods.org/pods/LLGeneral)
+
+## Example
+
+To run the example project, clone the repo, and run `pod install` from the Example directory first.
+
+## Requirements
+
+## Installation
+
+LLGeneral is available through [CocoaPods](https://cocoapods.org). To install
+it, simply add the following line to your Podfile:
+
+```ruby
+pod 'LLGeneral'
+```
+
+## Author
+
+ZHK1024, ZHK1024@foxmail.com
+
+## License
+
+LLGeneral is available under the MIT license. See the LICENSE file for more info.
