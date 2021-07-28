fe012f1de1f108bba83eea37c49cbf1dfb9a79dc
Ruris <ruris@qq.com>
2021-07-28 10:36:46 +0800

    Update NSAttributedString.swift

--- LLGeneral/Classes/Extensions/Foundation/NSAttributedString.swift
+++ LLGeneral/Classes/Extensions/Foundation/NSAttributedString.swift
@@ -14,7 +14,7 @@ extension NSAttributedString {
     /// - Parameter path: 文件路径
     /// - Throws:  异常信息
     /// - Returns: 富文本对象
-    static func fromRTF(path: String) throws -> NSAttributedString? {
+    public static func fromRTF(path: String) throws -> NSAttributedString? {
         try fromRTF(url: URL(fileURLWithPath: path))
     }
     
@@ -22,7 +22,7 @@ extension NSAttributedString {
     /// - Parameter url: 文件 URL
     /// - Throws:  异常信息
     /// - Returns: 富文本对象
-    static func fromRTF(url: URL) throws -> NSAttributedString {
+    public static func fromRTF(url: URL) throws -> NSAttributedString {
         try NSAttributedString(url: url, options: [
             NSAttributedString.DocumentReadingOptionKey.documentType : NSAttributedString.DocumentType.rtf
         ], documentAttributes: nil)
